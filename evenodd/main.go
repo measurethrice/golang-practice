82a4d8dff9308427d737960a70a73e61a3981c49
measurethrice <stevennery@gmail.com>
2020-08-30 13:59:48 -0700

    SN

---
+++ evenodd/main.go
@@ -0,0 +1,20 @@
+// This program generates a splice of ints and tests
+// if each element if the splice is even or odd. 
+
+package main
+
+import(
+	"fmt"
+)
+
+func main() { 
+	numbers := []int{0,1,2,3,4,5,6,7,8,9,10}
+
+	for _, number := range numbers {
+		if number % 2 == 0 {
+			fmt.Println(number, "is even")
+		} else {
+			fmt.Println(number, "is odd")
+		}
+	}
+}
