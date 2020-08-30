a95959dc0a52409865f0c9c9845439d5bb22f59d
measurethrice <stevennery@gmail.com>
2020-08-30 14:25:02 -0700

    SN

--- structs/main.go
+++ structs/main.go
@@ -2,13 +2,26 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	zipcode int
+}
+
 type person struct {
 	firstName string
 	lastName string
+	contact contactInfo
 }
 
 func main() {
-	alex := person{firstName: "Alex", lastName: "Anderson"}
-	fmt.Println(alex)
+	jim := person{
+		firstName: "Jim",
+		lastName: "Henson",
+		contact: contactInfo{
+			email: "jim@gmail.com",
+			zipcode: 87114,
+		},
+	}
 
+	fmt.Printf("%+v", jim)
 }
